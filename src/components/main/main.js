e7202344312764df5b5fbe3f66d61ff83bbe73a5
hana-bananaa <debartosiaki@gmail.com>
2021-09-28 20:57:39 -0300

    first commit

---
+++ src/components/main/main.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './main.css'
+
+
+function Main() {
+ 
+ 
+    return (
+        <div className="cards-container">
+            <div className="js1 total">
+              <div className="imagem">
+                  <a href="https://gifted-mcnulty-cc0887.netlify.app/" className="gb">
+                      <h3 className="titlecard">Drum Rock</h3>
+                   </a>
+               </div>
+            </div>
+
+            <div className="js2 total">
+              <div className="imagem">
+                  <a href="https://fervent-wozniak-392feb.netlify.app/" className="gb">
+                      <h3 className="titlecard">Let's Draw </h3>
+                  </a>
+              </div>
+            </div>
+            <div className="js3 total">
+                <div className="imagem">
+                    <a href="https://elastic-spence-9012c4.netlify.app/" className="gb">
+                        <h3 className="titlecard">Quizz do Milhão</h3>
+                    </a>
+                </div>
+            </div>
+
+      </div>
+       
+  );
+}
+
+  export default Main;
